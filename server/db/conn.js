687929668539168fb0c02a66d4fd90431f2915fa
Nate Chiles <nate.chiles@gmail.com>
2021-11-12 14:53:31 -0500

    css table updates

--- server/db/conn.js
+++ server/db/conn.js
@@ -10,10 +10,9 @@ var _db;
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
-      // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("machineDB");
+        _db = db.db("maintenanceDb");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
